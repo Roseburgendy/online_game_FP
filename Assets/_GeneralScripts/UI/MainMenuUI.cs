657a03a636432722d6fe522fd2ae9cb239b6fed7
neng <henjiongde@gmail.com>
2025-07-14 10:59:27 +0800

    Stashed Change
    yeah

--- Assets/_GeneralScripts/UI/MainMenuUI.cs
+++ Assets/_GeneralScripts/UI/MainMenuUI.cs
@@ -19,7 +19,9 @@ public class MainMenuUI : MonoBehaviour {
         });
         playSingleplayerButton.onClick.AddListener(() => {
             KitchenGameMultiplayer.playMultiplayer = false;
-            Loader.Load(Loader.Scene.LobbyScene);
+
+            Loader.Load(Loader.Scene.LXXCharacterSelectScene);
+
         });
         backButton.onClick.AddListener(() => {
             Loader.Load(Loader.Scene.TitleScene);
